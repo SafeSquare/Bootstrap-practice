9c7cabca1a25769c8da0d81ad2cc62a538862fcf
SafeSquare <ezeafulukwefredrickosita@gmail.com>
2022-05-16 09:45:46 +0100

    ch: Contd. work on the project

---
+++ script.jsx
@@ -0,0 +1,18 @@
+<script>
+<Swiper
+breakpoints={{
+// when window width is >= 640px
+425: {
+width: 425,
+slidesPerView: 1,
+},
+// when window width is >= 768px
+769: {
+width: 769,
+slidesPerView: 2,
+},
+}}
+>
+{slides}
+</Swiper> 
+</script>
